de6d377c38f4814e3ca8dbd01988d0e9fc7a9055
k3fanova@yandex.ru <katyaefanova222gmail.com>
2021-09-03 19:06:15 +0300

    Добавьте файлы проекта.

---
+++ OneProgramApp/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OneProgramApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            foreach (UIElement el in MainRoot.Children)
+            {
+                if (el is Button)
+                {
+                    ((Button)el).Click += Button_Click;
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string str = ((Button)e.OriginalSource).Content.ToString();
+
+            if (str == "AC")
+            {
+                tblCalculator.Text = "";
+            }
+
+            if (str == "=")
+            {
+                string value = new DataTable().Compute(tblCalculator.Text, null).ToString();
+                tblCalculator.Text = value;
+            }
+
+            else
+            {
+                tblCalculator.Text += str;
+            }
+        }
+    }
+}
